d4ce3cd697e0836c83621a152e340b1efe2a502e
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-02-20 00:17:10 +0530

    Add files via upload

---
+++ Built in Funciton in python 5.py
@@ -0,0 +1,33 @@
+print("Hello,wrold!")
+my_list=[1,2,3,4,5]
+print(len(my_list))
+
+print("input() function in python ")
+name=input("Enter your name")
+print("Hello,",name)
+
+print("range() function in python ")
+
+for i in range(5): 
+    print(i)
+numbers=[1,2,3,3,4]
+print(sum(numbers))
+numbers=[1,2,3,4,5]
+print("this is the number",numbers)
+
+print("max() function in python ")
+numbers=[1,2,3,4,5]
+print(min(numbers))
+
+num=-10
+print(abs(num))
+
+print('sorted fucntion in python')
+numbers=[5,6,4,6,6]
+sorted_numbers=sorted(numbers)
+print(sorted_numbers)
+
+
+print(type(5))
+print(type('Hello'))
+
